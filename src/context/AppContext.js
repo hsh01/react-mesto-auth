6c93bb01ee601fcdc4a7ebd3674cf465de9101cc
Shamil K <hsh0@ya.ru>
2022-08-03 00:14:26 +0500

    conv and clean

--- src/context/AppContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const AppContext = React.createContext({
-    loggedIn: false,
-    handleLogin: () => { },
-    userData: null,
-});
--- src/context/CurrentUserContext.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-// @ts-ignore
-export const CurrentUserContext = React.createContext();
