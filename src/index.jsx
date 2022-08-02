67a4b6e191d5c7dff6386d02253c6782aa5db2f9
Shamil K <hsh0@ya.ru>
2022-08-03 01:10:44 +0500

    gh pages

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
+
 document.title = 'Mesto';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <HashRouter>
+            <App/>
+        </HashRouter>
     </React.StrictMode>
 );
